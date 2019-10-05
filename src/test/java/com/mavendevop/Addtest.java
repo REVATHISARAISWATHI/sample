fc1c8921b555055b3e1c83b4bf5727f6bfcb352f
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-10-05 07:35:04 -0700

    Update Addtest.java

--- src/test/java/com/mavendevop/Addtest.java
+++ src/test/java/com/mavendevop/Addtest.java
@@ -11,8 +11,8 @@ public class Addtest {
 	public void test() {
 		Add a=new Add();
 		
-		assertEquals(6,a.Addf(2,4));
-		assertEquals(8,a.Addf(4,4));
+		assertEquals(6,a.addf(2,4));
+		assertEquals(8,a.addf(4,4));
 		
 
 	}
