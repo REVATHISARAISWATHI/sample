a7373a0ef8c3dcc39961fcba9a9fab7373d44e94
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-10-05 07:27:46 -0700

    Update Add.java

--- src/main/java/com/mavendevop/Add.java
+++ src/main/java/com/mavendevop/Add.java
@@ -1,16 +1,9 @@
 package com.mavendevop;
 
 public class Add {
-	public static int Addf(int a,int b)
+	public int Addf(int a,int b)
 	 {
 		return a+b;
 			}
-	public static void main(String args[])
-	{
-		int c=Addf(2,3);
-		System.out.println(c);
-		
-		
-	}
 
 }
